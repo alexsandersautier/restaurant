e3bc0c8625544920f596229477fdece0aa53d877
Alexsander Sautier <alexsanderesramos@gmail.com>
2024-08-22 21:21:22 -0300

    app implemented

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
+  const [quantity, setQuantity] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Pessoas no restaurante</Text>
+      <View style={styles.containerValue}>
+        <Text style={styles.value}>{quantity}</Text>  
+      </View>
+      <View style={styles.containerButtons}>
+        <Pressable 
+          style={styles.button}
+          onPress={() => {setQuantity((prev) =>{
+            if (prev + 1 > 10) {
+              alert("Restaurante chegou no seu limite");
+              return prev;
+            } else {
+              return prev + 1;
+            }
+          })}}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </Pressable>
+        <Pressable 
+          style={styles.button}
+          onPress={() => {setQuantity((prev) => {
+            if ( prev - 1 <= 0) {
+              return 0;
+            } else {
+              return prev - 1
+            }
+          })}}>
+          <Text style={styles.buttonText}>Remover</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +46,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10
   },
+  containerValue:{
+    backgroundColor: "black",
+    height: 60,
+    width: 40,
+    justifyContent: 'center',
+    alignItems: "center",
+    borderRadius: 10
+  },
+  value: {
+    color: "white",
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  containerButtons: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    gap: 20
+  },
+  button: {
+    backgroundColor: "#0D7EF9",
+    height: 30,
+    width: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold"
+  }
 });
